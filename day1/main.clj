22d85f6a19fb5506104eb62ccca501b5384ec436
Makar Solomatin <makar.solomatin.5@yandex.ru>
2024-12-01 18:17:52 +0300

    Update day1 solution

--- day1/main.clj
+++ day1/main.clj
@@ -9,36 +9,19 @@
         second-list (sort (map (comp Integer/parseInt second) pairs))]
     [first-list second-list]))
 
-(defn total-distance
-  "Calculates total distance between two *sorted* lists of the *same size*"
-  ([first-list second-list] (total-distance first-list second-list 0))
-  ([first-list second-list total]
-   (if (empty? first-list)
-     total
-     (total-distance (rest first-list)
-                     (rest second-list)
-                     (+ total
-                        (abs (- (first first-list) (first second-list))))))))
-
-(defn similarity
-  "Counts up total similarity between two lists"
-  ([coll freq] (similarity coll freq 0))
-  ([coll freq total]
-   (if (empty? coll)
-     total
-     (similarity (rest coll)
-                 freq
-                 (+ total (* (first coll) (get freq (first coll) 0)))))))
-
 ; First part of Day 1's task
-(defn aoc1 [filename] (apply total-distance (split-lists filename)))
+(defn aoc1
+  [filename]
+  (apply +
+    (map #(abs (- (first %) (second %)))
+      (apply map vector (split-lists filename)))))
 
 ; Second part of Day 1's task
 (defn aoc2
   [filename]
-  (let [[first-list second-list] (split-lists filename)]
-    (similarity (set first-list) (frequencies second-list))))
+  (let [[first-list second-list] (split-lists filename)
+        freq (frequencies second-list)]
+    (apply + (map #(* % (get freq % 0)) first-list))))
 
 (aoc1 "input.txt")
-
 (aoc2 "input.txt")
