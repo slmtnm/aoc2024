6447aa49a787e6b0d95ec9cf0779cb920d379dc2
Makar Solomatin <m.solomatin@tinkoff.ru>
2024-12-03 20:31:08 +0300

    Done day 3

---
+++ day3/main.clj
@@ -0,0 +1,28 @@
+(require '[clojure.string :as str])
+
+; Pattern of "mul(x, y)" expression, where x and y are integer numbers
+(def mulpat (re-pattern #"mul\(([0-9]+),([0-9]+)\)"))
+
+; Pattern of "don't()......do()" expression
+(def dontpat (re-pattern #"(don't\(\)).*?(do\(\)|$)"))
+
+(defn execute
+  "Executes given code"
+  [code]
+  (->> code
+       (re-seq mulpat)
+       (map rest)
+       (map #(map Integer/parseInt %))
+       (map (partial apply *))
+       (reduce +)))
+
+; First task of Day 3
+(->> (slurp "input.txt")
+     execute)
+
+; Second task of Day 3
+(->> (slurp "input.txt")
+     ((comp str/split-lines str/join))     ; remove newlines from code
+     ((fn [s] (str/replace s dontpat ""))) ; remove don't()...do() blocks
+     execute)
+
