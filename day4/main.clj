b8481d1b8a0c8491926eae68778c593fa8d72dd4
Makar Solomatin <m.solomatin@tinkoff.ru>
2024-12-05 00:37:05 +0300

    Done day 4

--- day4/main.clj
+++ day4/main.clj
@@ -1,17 +1,29 @@
 (require '[clojure.string :as str])
 
+(defn read-matrix
+  "Reads symbols matrix from file"
+  [filename]
+  (->> (slurp filename)
+       str/split-lines
+       (map vec) ; convert each line to vector of character
+       vec))
+
 (defn get-2d "Get value of m[x][y]" [m [x y]] ((m y) x))
 
-(defn inbound
+(defn valid
+  "Checks whether (x, y) is a valid point in `m`"
   [m [x y]]
   (and (>= x 0) (>= y 0) (< y (count m)) (< x (count (m 0)))))
 
 (defn check-xmas
+  "Checks whether given points in `m` are constructing word XMAS"
   [m points]
-  (and (every? (partial inbound m) points)
+  (and (every? (partial valid m) points)
        (= "XMAS" (apply str (map #(get-2d m %) points)))))
 
 (defn xmas-directions
+  "Creates 8 points arrays of length 4,
+   for each direction (2 for verical, 4 for diagonal, 2 for horizontal)"
   [[x y]]
   [[[x y] [(+ x 1) y] [(+ x 2) y] [(+ x 3) y]]
    [[x y] [(- x 1) y] [(- x 2) y] [(- x 3) y]]
@@ -23,42 +35,49 @@
    [[x y] [(+ x 1) (+ y 1)] [(+ x 2) (+ y 2)] [(+ x 3) (+ y 3)]]])
 
 (defn count-xmas
+  "Counts number of XMAS words in given matrix"
   [m]
-  (count (filter identity
-           (flatten (for [x (range 0 (count (m 0)))
-                          y (range 0 (count m))]
-                      (map #(check-xmas m %) (xmas-directions [x y])))))))
+  (let [width (count (m 0))
+        height (count m)]
+    (->> (for [x (range width)
+               y (range height)]
+           (map #(check-xmas m %) (xmas-directions [x y])))
+         flatten
+         (filter identity)
+         count)))
 
 (defn get-main-diag
+  "Returns points vector of length 3 of main diagonal
+   with center located in given coordinates"
   [m x y]
   [(get-2d m [(- x 1) (- y 1)]) (get-2d m [x y]) (get-2d m [(+ x 1) (+ y 1)])])
+
 (defn get-sec-diag
+  "Returns points vector of length 3 of secondary diagonal
+   with center located in given coordinates"
   [m x y]
   [(get-2d m [(+ x 1) (- y 1)]) (get-2d m [x y]) (get-2d m [(- x 1) (+ y 1)])])
-(defn is-mas
+
+(defn check-mas
+  "Checks wheter there are two MAS/SAM words in main and secondary diagonal
+   with center located in given coordinates"
   [m [x y]]
   (let [main-diag-str (apply str (get-main-diag m x y))
         sec-diag-str (apply str (get-sec-diag m x y))]
     (and (or (= main-diag-str "MAS") (= main-diag-str "SAM"))
          (or (= sec-diag-str "MAS") (= sec-diag-str "SAM")))))
 
-(defn for-mas
+(defn count-mas
+  "Counts number of MAS/SAM crosses in given matrix"
   [m]
-  (map #(is-mas m %)
-    (for [x (range 1 (- (count (m 0)) 1)) y (range 1 (- (count m) 1))] [x y])))
+  (let [width (count (m 0))
+        height (count m)]
+    (->> (for [x (range 1 (- width 1)) y (range 1 (- height 1))] [x y])
+         (filter #(check-mas m %))
+         count)))
 
 ; First task of Day 4
-(->> (slurp "input.txt")
-     str/split-lines
-     (map vec) ; convert each line to vector of character
-     vec
-     count-xmas)
+(count-xmas (read-matrix "input.txt"))
 
 ; Second task of Day 4
-(->> (slurp "input.txt")
-     str/split-lines
-     (map vec) ; convert each line to vector of character
-     vec
-     for-mas
-     (filter identity)
-     count)
+(count-mas (read-matrix "input.txt"))
